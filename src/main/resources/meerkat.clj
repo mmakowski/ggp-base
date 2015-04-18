bf374bc9692b65aab1be618f18ae49dd71a1e1f5
Maciek Makowski <maciek.makowski@gmail.com>
2015-04-18 22:47:12 +0100

    compulsive deliberation for small single-player games

--- src/main/resources/meerkat.clj
+++ src/main/resources/meerkat.clj
@@ -9,6 +9,34 @@
         random-move   (.getRandomMove state-machine current-state role)]
     random-move))
 
+(defn compulsive-deliberation [gamer]
+  (do
+    (assert (= (count (.getRoles (.getStateMachine gamer))) 1))
+    (let [state-machine (.getStateMachine gamer)
+          current-state (.getCurrentState gamer)
+          role          (.getRole gamer)
+
+          make-move     (fn [state move] (.getNextState state-machine state [move]))]
+      (letfn [(max-score [state]
+                (if (.isTerminal state-machine state)
+                  (.getGoal state-machine state role)
+                  (let [moves  (.getLegalMoves state-machine state role)
+                        scores (map (comp max-score (partial make-move state)) moves)]
+                    (reduce max 0 scores))))]
+        (let [higher-score (fn [curr-max candidate]
+                         (if (< (:score curr-max) (:score candidate))
+                           candidate
+                           curr-max))
+
+              with-score (fn [move]
+                           (let [new-state (make-move current-state move)]
+                             {:move move :score (max-score new-state)}))
+
+              moves         (.getLegalMoves state-machine current-state role)
+              scored-moves  (map with-score moves)
+              best-move-and-score (reduce higher-score (first scored-moves) scored-moves)]
+          (:move best-move-and-score))))))
+
 (defn MeerkatGamer []
   (proxy [StateMachineGamer] []
 
@@ -16,7 +44,7 @@
       (ProverStateMachine.))
 
     (stateMachineSelectMove [timeout]
-      (random-move this))
+      (compulsive-deliberation this))
 
     (stateMachineMetaGame [timeout]
       (println "MeerkatGamer metagame called"))
